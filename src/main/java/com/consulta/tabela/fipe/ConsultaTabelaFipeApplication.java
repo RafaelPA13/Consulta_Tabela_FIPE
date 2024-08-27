32fa776ad277c28dea29ab65fbfa5369a9f0ab53
RafaelPA13 <rafaporann@gmail.com>
2024-08-27 15:21:52 -0300

    Commit inicial

---
+++ src/main/java/com/consulta/tabela/fipe/ConsultaTabelaFipeApplication.java
@@ -0,0 +1,13 @@
+package com.consulta.tabela.fipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaTabelaFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaTabelaFipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consulta/tabela/fipe/ConsultaTabelaFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consulta.tabela.fipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaTabelaFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
